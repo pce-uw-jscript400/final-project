7a4a5e908d3b8eb460064c5997694e2f7e5acade
Alycia Cahill <acahil@uw.edu>
2019-08-28 09:15:00 -0700

    Just in case the zombies get me while I am camping

--- final-frontend/src/api/users.js
+++ final-frontend/src/api/users.js
@@ -4,7 +4,6 @@ export const fetchUsers = () => request('/api/users')
 
 export const updateUsername = ({ user, name }) => {
     const path = `/api/users/${user._id}/edit`
-    //console.log(`ALYLOG:${JSON.stringify(name)}`)
     const options = { body:{'name': name}, method: 'PUT' }
     return request(path, options)
   }
--- final-frontend/src/components/auth/Signup.Form.js
+++ final-frontend/src/components/auth/Signup.Form.js
@@ -1,13 +1,89 @@
 import React from 'react'
-import Form from './Form'
+import { withRouter } from 'react-router'
 
-export default ({ onSubmit }) => (
-  <main className='container'>
-    <section className='row justify-content-md-center'>
+class Form extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange ({ target: { name, value } }) {
+    this.setState({ [name]: value })
+  }
+
+
+  handleSubmit (e) {
+    e.preventDefault()
+    this.props.onSubmit(this.state)
+      .then(() => this.props.history.push('/users'))
+  }
+
+  render () {
+    return (
+      <main className='container'>
+      <section className='row justify-content-md-center'>
       <div className='col col-lg-5'>
-        <h1>Signup</h1>
-        <Form onSubmit={onSubmit} />
+          <h1>Login</h1>
+      <form onSubmit={this.handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='username'>Username</label>
+          <input
+            className='form-control'
+            id='username'
+            onChange={this.handleChange}
+            name='username'
+            type='text'
+            value={this.state.username}
+            required />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            className='form-control'
+            id='password'
+            onChange={this.handleChange}
+            name='password'
+            type='password'
+            value={this.state.password}
+            required  />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='first-name'>First Name</label>
+          <input
+            className='form-control'
+            id='first-name'
+            onChange={this.handleChange}
+            name='fname'
+            type='text'
+            value={this.state.firstname}
+            required  />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='last-name'>Last Name</label>
+          <input
+            className='form-control'
+            id="last-name"
+            onChange={this.handleChange}
+            name='lname'
+            type='text'
+            value={this.state.lastname}
+            required  />
+        </div>
+        <button type='submit' className='btn btn-primary'>Submit</button>
+      </form>
       </div>
-    </section>
-  </main>
-)
+      </section>
+      </main>
+    )
+  }
+}
+
+export default withRouter(SignUpForm)
